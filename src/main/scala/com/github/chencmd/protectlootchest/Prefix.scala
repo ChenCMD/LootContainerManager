fd55724265de8a0c5e6d2d799643dcbfb208a81f
ChenCMD <scnmed.g@gmail.com>
2022-04-20 21:03:27 +0900

    :technologist: add chat color utility object

---
+++ src/main/scala/com/github/chencmd/protectlootchest/Prefix.scala
@@ -0,0 +1,10 @@
+package com.github.chencmd.protectlootchest
+
+import org.bukkit.ChatColor
+
+object Prefix {
+  final val ERROR = s"${ChatColor.RED}Error >> ${ChatColor.RESET}"
+  final val WARNING = s"${ChatColor.YELLOW}Warning >> ${ChatColor.RESET}"
+  final val SUCCESS = s"${ChatColor.GREEN}Success >> ${ChatColor.RESET}"
+  final val INFO = s"${ChatColor.AQUA}Information >> ${ChatColor.RESET}"
+}
