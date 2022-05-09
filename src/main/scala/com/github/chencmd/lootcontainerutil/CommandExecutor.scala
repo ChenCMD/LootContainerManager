d18666b7f6aec8f0c5f9ebcbee0bb66c5bd4c909
ChenCMD <scnmed.g@gmail.com>
2022-05-10 03:29:18 +0900

    Revert ":construction: a"
    This reverts commit f2dbc334d21137eb18bc68f2f28d7b7833f157ee.

--- src/main/scala/com/github/chencmd/lootcontainerutil/CommandExecutor.scala
+++ src/main/scala/com/github/chencmd/lootcontainerutil/CommandExecutor.scala
@@ -6,10 +6,10 @@ import cats.effect.unsafe.implicits.global
 import com.github.chencmd.lootcontainerutil.Prefix
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
-import com.github.chencmd.lootcontainerutil.generic.extensions.CastOps.downcastOrNone
-import com.github.chencmd.lootcontainerutil.minecraft.OnMinecraftThread
 
-class CommandExecutor(ignorePlayerSet: IgnorePlayerSet)(using mcThread: OnMinecraftThread[IO]) {
+import com.github.chencmd.lootcontainerutil.generic.extensions.CastOps.downcastOrNone
+
+class CommandExecutor(ignorePlayerSet: IgnorePlayerSet) {
   def run(sender: CommandSender, args: Array[String]): IO[Boolean] = {
     val p: Option[Player] = sender.downcastOrNone[Player]
 
@@ -25,11 +25,6 @@ class CommandExecutor(ignorePlayerSet: IgnorePlayerSet)(using mcThread: OnMinecr
           _ <- OptionT.liftF(IO {
             p.sendMessage(s"${Prefix.SUCCESS}ルートコンテナーの保護を8秒間無効化しました。")
           })
-        } yield true
-      case "gen_asset" =>
-        for {
-          p <- OptionT.fromOption[IO](p)
-          _ <- OptionT.liftF(GenLootAsset.generateLootAsset(p))
         } yield true
       case _ => OptionT.pure[IO](false)
     }
