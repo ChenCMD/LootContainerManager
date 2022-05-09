dc25d1ebe33a9b71916555b951de527a4c93809a
ChenCMD <scnmed.g@gmail.com>
2022-05-09 19:12:09 +0900

    :construction: ProtectActionListenerを仮作成

--- src/main/scala/com/github/chencmd/lootcontainerutil/ProtectActionListener.scala
+++ src/main/scala/com/github/chencmd/lootcontainerutil/ProtectActionListener.scala
@@ -7,36 +7,68 @@ import org.bukkit.{Bukkit, ChatColor}
 import org.bukkit.block.Container
 import org.bukkit.entity.Player
 import org.bukkit.event.inventory.InventoryOpenEvent
+import org.bukkit.event.world.LootGenerateEvent
 import org.bukkit.event.{EventHandler, Listener}
 import org.bukkit.loot.{LootTable, Lootable}
+import org.bukkit.metadata.{FixedMetadataValue, MetadataValue}
 import org.bukkit.plugin.java.JavaPlugin
+
+import scala.util.chaining.*
+import scala.jdk.CollectionConverters.*
 
 class ProtectActionListener(plugin: JavaPlugin, ignorePlayerSet: IgnorePlayerSet) extends Listener {
   Bukkit.getPluginManager.registerEvents(this, plugin)
 
-  @EventHandler def onContainerOpen(e: InventoryOpenEvent): Unit = {
-    val setLootTable: Option[LootTable] = Option(e.getInventory.getHolder)
-      .flatMap(_.downcastOrNone[Container])
-      .flatMap(_.downcastOrNone[Lootable])
-      .flatMap(inv => Option(inv.getLootTable))
+  @EventHandler def onLootGenerate(e: LootGenerateEvent): Unit = {
+    val pOpt = e.getEntity.downcastOrNone[Player]
 
-    if (setLootTable.isEmpty) {
+    if (pOpt.isEmpty) {
       return
     }
 
-    val p = e.getPlayer.asInstanceOf[Player]
+    val p: Player = pOpt.get
 
     val action = for {
       isIgnoreProtect <- ignorePlayerSet.isIgnorePlayer(p)
-      _ <- IO.whenA(!isIgnoreProtect)(IO {
-        p.sendMessage(s"${Prefix.INFO}ルートテーブルが設定されているため開くことができませんでした。")
-        p.sendMessage(s"${Prefix.INFO}意図して開く場合は、${ChatColor.GOLD}/lcu ignore${ChatColor.WHITE}を実行してください。")
-        p.sendMessage(s"${Prefix.INFO}設定されているルートテーブル: ${setLootTable.get.getKey}")
-      })
-    } yield isIgnoreProtect
 
-    val isIgnoreProtect = action.unsafeRunSync()
-    if (!isIgnoreProtect) {
+      _ <- IO.unlessA(isIgnoreProtect) {
+        val lootTableKey = e.getLootTable.getKey
+        val cmd = e.getLootContext
+          .getLocation
+          .getBlock
+          .pipe(l => s"data modify block ${l.getX} ${l.getY} ${l.getZ} LootTable set value \"$lootTableKey\"")
+
+        IO {
+          Bukkit.dispatchCommand(p, cmd)
+
+          p.sendMessage(s"${Prefix.INFO}ルートテーブルが設定されているため開くことができませんでした。")
+          p.sendMessage(s"${Prefix.INFO}意図して開く場合は、${ChatColor.GOLD}/lcu ignore${ChatColor.WHITE}を実行してください。")
+          p.sendMessage(s"${Prefix.INFO}設定されているルートテーブル: $lootTableKey")
+
+          p.setMetadata("generateCancelled", FixedMetadataValue(plugin, Bukkit.getWorlds.asScala.head.getGameTime))
+        }
+      }
+    } yield ()
+
+    action.unsafeRunSync()
+  }
+
+  @EventHandler def onContainerOpen(e: InventoryOpenEvent): Unit = {
+
+    val p = e.getPlayer.asInstanceOf[Player]
+
+    val action = IO {
+      val res = p.getMetadata("generateCancelled")
+        .asScala
+        .headOption
+        .flatMap(_.value.downcastOrNone[Long])
+        .exists(Bukkit.getWorlds.asScala.head.getGameTime - _ <= 10)
+      p.removeMetadata("generateCancelled", plugin)
+      res
+    }
+
+    val isCancelled = action.unsafeRunSync()
+    if (isCancelled) {
       e.setCancelled(true)
     }
   }
