41447da930f8a66c844d5ee9925152f0e9ee93ef
ChenCMD <scnmed.g@gmail.com>
2022-05-10 04:45:44 +0900

    :adhesive_bandage: Fix Container protect

--- src/main/scala/com/github/chencmd/lootcontainerutil/ProtectActionListener.scala
+++ src/main/scala/com/github/chencmd/lootcontainerutil/ProtectActionListener.scala
@@ -1,8 +1,9 @@
 package com.github.chencmd.lootcontainerutil
 
-import cats.effect.IO
+import cats.effect.{IO, SyncIO}
 import cats.effect.unsafe.implicits.global
 import com.github.chencmd.lootcontainerutil.generic.extensions.CastOps.downcastOrNone
+import com.github.chencmd.lootcontainerutil.minecraft.OnMinecraftThread
 import org.bukkit.{Bukkit, ChatColor}
 import org.bukkit.block.Container
 import org.bukkit.entity.Player
@@ -16,7 +17,7 @@ import org.bukkit.plugin.java.JavaPlugin
 import scala.util.chaining.*
 import scala.jdk.CollectionConverters.*
 
-class ProtectActionListener(plugin: JavaPlugin, ignorePlayerSet: IgnorePlayerSet) extends Listener {
+class ProtectActionListener(plugin: JavaPlugin, ignorePlayerSet: IgnorePlayerSet)(using mcThread: OnMinecraftThread[IO]) extends Listener {
   Bukkit.getPluginManager.registerEvents(this, plugin)
 
   @EventHandler def onLootGenerate(e: LootGenerateEvent): Unit = {
@@ -38,15 +39,16 @@ class ProtectActionListener(plugin: JavaPlugin, ignorePlayerSet: IgnorePlayerSet
           .getBlock
           .pipe(l => s"data modify block ${l.getX} ${l.getY} ${l.getZ} LootTable set value \"$lootTableKey\"")
 
-        IO {
-          Bukkit.dispatchCommand(p, cmd)
+        for {
+          _ <- mcThread.runAndForget(SyncIO(Bukkit.dispatchCommand(Bukkit.getConsoleSender, cmd)))
+          _ <- IO {
+            p.sendMessage(s"${Prefix.INFO}ルートテーブルが設定されているため開くことができませんでした。")
+            p.sendMessage(s"${Prefix.INFO}意図して開く場合は、${ChatColor.GOLD}/lcu ignore${ChatColor.WHITE}を実行してください。")
+            p.sendMessage(s"${Prefix.INFO}設定されているルートテーブル: $lootTableKey")
 
-          p.sendMessage(s"${Prefix.INFO}ルートテーブルが設定されているため開くことができませんでした。")
-          p.sendMessage(s"${Prefix.INFO}意図して開く場合は、${ChatColor.GOLD}/lcu ignore${ChatColor.WHITE}を実行してください。")
-          p.sendMessage(s"${Prefix.INFO}設定されているルートテーブル: $lootTableKey")
-
-          p.setMetadata("generateCancelled", FixedMetadataValue(plugin, Bukkit.getWorlds.asScala.head.getGameTime))
-        }
+            p.setMetadata("generateCancelled", FixedMetadataValue(plugin, Bukkit.getWorlds.asScala.head.getGameTime))
+          }
+        } yield ()
       }
     } yield ()
 
