6fed43661ae47e040a7a80118fca34b7e8eb0dc6
ChenCMD <scnmed.g@gmail.com>
2022-05-11 15:47:54 +0900

    :construction: /lcu gen_assetの処理を半分作成

--- src/main/scala/com/github/chencmd/lootcontainerutil/CommandExecutor.scala
+++ src/main/scala/com/github/chencmd/lootcontainerutil/CommandExecutor.scala
@@ -6,29 +6,43 @@ import cats.effect.unsafe.implicits.global
 import com.github.chencmd.lootcontainerutil.Prefix
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
-
 import com.github.chencmd.lootcontainerutil.generic.extensions.CastOps.downcastOrNone
+import com.github.chencmd.lootcontainerutil.minecraft.OnMinecraftThread
+import de.tr7zw.nbtapi.NbtApiException
+import org.bukkit.Bukkit
 
-class CommandExecutor(ignorePlayerSet: IgnorePlayerSet) {
-  def run(sender: CommandSender, args: Array[String]): IO[Boolean] = {
+class CommandExecutor(ignorePlayerSet: IgnorePlayerSet)(using mcThread: OnMinecraftThread[IO]) {
+  def run(sender: CommandSender, args: Array[String]): Boolean = {
     val p: Option[Player] = sender.downcastOrNone[Player]
 
     if (args.length == 0) {
-      return IO.whenA(sender.isOp)(IO(sender.sendMessage(s"${Prefix.ERROR}/lcu <ignore>"))).as(sender.isOp)
+      return IO.whenA(sender.isOp)(IO(sender.sendMessage(s"${Prefix.ERROR}/lcu <ignore>")))
+        .as(sender.isOp)
+        .unsafeRunSync()
     }
 
-    val optIO = args(0) match {
+    args(0) match {
       case "ignore" =>
-        for {
+        val action = for {
           p <- OptionT.fromOption[IO](p)
           _ <- OptionT.liftF(ignorePlayerSet.registerIgnorePlayer(p))
           _ <- OptionT.liftF(IO {
             p.sendMessage(s"${Prefix.SUCCESS}ルートコンテナーの保護を8秒間無効化しました。")
           })
         } yield true
-      case _ => OptionT.pure[IO](false)
+        action.value.map(_.getOrElse(false)).unsafeRunSync()
+      case "gen_asset" =>
+        val action = for {
+          p <- OptionT.fromOption[IO](p)
+          _ <- OptionT.liftF(GenLootAsset.generateLootAsset(p))
+        } yield ()
+        action.value.unsafeRunAsync(
+          _.left
+            .toOption
+            .foreach(_.printStackTrace())
+        )
+        true
+      case _ => false
     }
-
-    optIO.value.map(_.getOrElse(true))
   }
 }
---
+++ src/main/scala/com/github/chencmd/lootcontainerutil/GenLootAsset.scala
@@ -0,0 +1,52 @@
+package com.github.chencmd.lootcontainerutil
+
+import cats.effect.{IO, SyncIO}
+import cats.data.OptionT
+import minecraft.OnMinecraftThread
+import generic.extensions.CastOps.downcastOrNone
+import org.bukkit.{Bukkit, Location}
+import org.bukkit.block.{Block, Container}
+import org.bukkit.entity.Player
+import org.bukkit.loot.Lootable
+
+import scala.jdk.CollectionConverters.*
+import de.tr7zw.nbtapi.NBTTileEntity
+
+object GenLootAsset {
+  def generateLootAsset(p: Player)(using mcThread: OnMinecraftThread[IO]): IO[Unit] = {
+    val acc = 5
+
+    val action = mcThread.run(SyncIO {
+      val loc = p.getEyeLocation
+      val vec = loc.getDirection.normalize().multiply(1d / acc)
+
+      (0 until (5 * acc))
+        .view
+        .map(i => loc.clone() add (vec.clone() multiply i))
+        .map(p.getWorld.getBlockAt)
+        .flatMap(_.getState.downcastOrNone[Container])
+        .headOption
+        .foreach { (block: Container) =>
+          for (item <- NBTTileEntity(block).getCompoundList("Items").asScala.toList) yield {
+            val id = item.getString("id")
+            val slot = item.getByte("Slot")
+            val count = item.getByte("Count")
+            val tag = Option(item.getCompound("tag"))
+            p.sendMessage(id)
+
+            /*
+             * "tag.TSB.ID"           -> "{id:%%tag.TSB.ID%%}"
+             * "tag.TSB{Currency:1b}" -> "{Count:%%Count%%,PresetItem:\"currency/\"}"
+             * "tag.TSB{Currency:2b}" -> "{Count:%%Count%%,PresetItem:\"currency/high\"}"
+             * "tag.TSB.ShardRarity"  -> "{Count:%%Count%%,PresetItem:\"sacred_shard/lv-%%tag.TSB.ShardRarity%%\"}"
+             * "{}"                   -> "{Count:%%Count%%,id:%%id%%}"
+             */
+            // TODO Configデータを読み込み、NBTPathをパースしてそれを元にデータを構築する
+          }
+        }
+    })
+
+
+    action.void
+  }
+}
