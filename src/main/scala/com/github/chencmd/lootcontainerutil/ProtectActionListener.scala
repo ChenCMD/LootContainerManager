4c26b6174d053e3a0a53dd652dfaac6893cfb8d0
ChenCMD <scnmed.g@gmail.com>
2022-05-17 04:19:28 +0900

    :adhesive_bandage: チャット欄に保護のログを出さないように

--- src/main/scala/com/github/chencmd/lootcontainerutil/ProtectActionListener.scala
+++ src/main/scala/com/github/chencmd/lootcontainerutil/ProtectActionListener.scala
@@ -4,7 +4,7 @@ import cats.effect.{IO, SyncIO}
 import cats.effect.unsafe.implicits.global
 import com.github.chencmd.lootcontainerutil.generic.extensions.CastOps.downcastOrNone
 import com.github.chencmd.lootcontainerutil.minecraft.OnMinecraftThread
-import org.bukkit.{Bukkit, ChatColor}
+import org.bukkit.{Bukkit, ChatColor, GameRule}
 import org.bukkit.block.Container
 import org.bukkit.entity.Player
 import org.bukkit.event.inventory.InventoryOpenEvent
@@ -34,13 +34,19 @@ class ProtectActionListener(plugin: JavaPlugin, ignorePlayerSet: IgnorePlayerSet
 
       _ <- IO.unlessA(isIgnoreProtect) {
         val lootTableKey = e.getLootTable.getKey
+        val w = e.getLootContext.getLocation.getWorld
         val cmd = e.getLootContext
           .getLocation
           .getBlock
           .pipe(l => s"data modify block ${l.getX} ${l.getY} ${l.getZ} LootTable set value \"$lootTableKey\"")
 
         for {
-          _ <- mcThread.runAndForget(SyncIO(Bukkit.dispatchCommand(Bukkit.getConsoleSender, cmd)))
+          _ <- mcThread.runAndForget(SyncIO {
+            val scfDefault = w.getGameRuleValue(GameRule.SEND_COMMAND_FEEDBACK)
+            w.setGameRule(GameRule.SEND_COMMAND_FEEDBACK, false)
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender, cmd)
+            w.setGameRule(GameRule.SEND_COMMAND_FEEDBACK, scfDefault)
+          })
           _ <- IO {
             p.sendMessage(s"${Prefix.INFO}ルートテーブルが設定されているため開くことができませんでした。")
             p.sendMessage(s"${Prefix.INFO}意図して開く場合は、${ChatColor.GOLD}/lcu ignore${ChatColor.WHITE}を実行してください。")
@@ -50,9 +56,12 @@ class ProtectActionListener(plugin: JavaPlugin, ignorePlayerSet: IgnorePlayerSet
           }
         } yield ()
       }
-    } yield ()
+    } yield isIgnoreProtect
 
-    action.unsafeRunSync()
+    val isOpenable = action.unsafeRunSync()
+    if (!isOpenable) {
+      e.setCancelled(true)
+    }
   }
 
   @EventHandler def onContainerOpen(e: InventoryOpenEvent): Unit = {
