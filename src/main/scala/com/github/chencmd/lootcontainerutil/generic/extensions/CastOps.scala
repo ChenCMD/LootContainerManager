9e8bf7079f60cf4806d66f60ef7156c3bb6791c7
ChenCMD <scnmed.g@gmail.com>
2022-05-21 03:24:23 +0900

    :label: downcastOrNoneの返り値の型を修正

--- src/main/scala/com/github/chencmd/lootcontainerutil/generic/extensions/CastOps.scala
+++ src/main/scala/com/github/chencmd/lootcontainerutil/generic/extensions/CastOps.scala
@@ -4,7 +4,7 @@ import scala.reflect.TypeTest
 
 object CastOps {
   extension[A] (value: A) {
-    def downcastOrNone[B](using tt: TypeTest[A, B]): Option[B] =
+    def downcastOrNone[B](using tt: TypeTest[A, B]): Option[A & B] =
       value match {
         case tt(b) => Some(b)
         case _ => None
