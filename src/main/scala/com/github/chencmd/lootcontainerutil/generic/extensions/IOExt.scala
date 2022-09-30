689343333d23547f63d4f5d8ca5732c2dbe7d7ef
ChenCMD <scnmed.g@gmail.com>
2022-09-30 20:41:47 +0900

    :technologist: IOに拡張メソッドを追加

---
+++ src/main/scala/com/github/chencmd/lootcontainerutil/generic/extensions/IOExt.scala
@@ -0,0 +1,15 @@
+package com.github.chencmd.lootcontainerutil.generic.extensions
+
+import cats.effect.IO
+import cats.effect.unsafe.IORuntime
+
+object IOExt {
+  extension [A](io: IO[A]) {
+    def unsafeRunHereSync()(using runtime: IORuntime): A = {
+      io.syncStep.unsafeRunSync() match {
+        case Left(v) => v.unsafeRunSync()
+        case Right(v) => v
+      }
+    }
+  }
+}
