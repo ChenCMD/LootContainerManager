f1804e80233d19bcaf41afb4af5ca5f610fe8602
ChenCMD <scnmed.g@gmail.com>
2024-01-28 18:49:57 +0900

    :adhesive_bandage: unsafeRunHereSync の実装を修正

--- src/main/scala/com/github/chencmd/lootcontainerutil/generic/extensions/IOExt.scala
+++ src/main/scala/com/github/chencmd/lootcontainerutil/generic/extensions/IOExt.scala
@@ -6,7 +6,7 @@ import cats.effect.unsafe.IORuntime
 object IOExt {
   extension [A](io: IO[A]) {
     def unsafeRunHereSync()(using runtime: IORuntime): A = {
-      io.syncStep(1).unsafeRunSync() match {
+      io.syncStep(Int.MaxValue).unsafeRunSync() match {
         case Left(v)  => v.unsafeRunSync()
         case Right(v) => v
       }
