60df5f1c7ec3d15761e386cc7a581b5f97f1fd4a
ChenCMD <scnmed.g@gmail.com>
2024-02-13 02:20:46 +0900

    :white_check_mark: テストを追加

---
+++ src/test/scala/nbt/NBTTagParserTest.scala
@@ -0,0 +1,128 @@
+package nbt
+
+import com.github.chencmd.lootcontainerutil.nbt.NBTTagParser
+import com.github.chencmd.lootcontainerutil.nbt.definition.NBTTag
+import org.scalatest.funspec.AnyFunSpec
+
+import scala.language.adhocExtensions
+import com.github.chencmd.lootcontainerutil.nbt.definition.NBTNel
+import cats.data.NonEmptyList
+
+class NBTTagParserTest extends AnyFunSpec {
+  import NBTTag.*
+  describe("NBTTagParser") {
+    it("should parse NBTTagByte") {
+      val input    = "{key:1b}"
+      val expected = NBTTagCompound(Map("key" -> NBTTagByte(1)))
+      val actual   = NBTTagParser.parse(input)
+      assert(actual == Right(expected))
+    }
+    it("should parse NBTTagShort") {
+      val input    = "{key:1s}"
+      val expected = NBTTagCompound(Map("key" -> NBTTagShort(1)))
+      val actual   = NBTTagParser.parse(input)
+      assert(actual == Right(expected))
+    }
+    it("should parse NBTTagInt") {
+      val input    = "{key:1}"
+      val expected = NBTTagCompound(Map("key" -> NBTTagInt(1)))
+      val actual   = NBTTagParser.parse(input)
+      assert(actual == Right(expected))
+    }
+    it("should parse NBTTagLong") {
+      val input    = "{key:1L}"
+      val expected = NBTTagCompound(Map("key" -> NBTTagLong(1)))
+      val actual   = NBTTagParser.parse(input)
+      assert(actual == Right(expected))
+    }
+    it("should parse NBTTagFloat") {
+      val input    = "{key:1.0f}"
+      val expected = NBTTagCompound(Map("key" -> NBTTagFloat(1.0f)))
+      val actual   = NBTTagParser.parse(input)
+      assert(actual == Right(expected))
+    }
+    it("should parse NBTTagFloat without integer part") {
+      val input    = "{key:.25f}"
+      val expected = NBTTagCompound(Map("key" -> NBTTagFloat(0.25f)))
+      val actual   = NBTTagParser.parse(input)
+      assert(actual == Right(expected))
+    }
+    it("should parse NBTTagDouble") {
+      val input    = "{key:1.0d}"
+      val expected = NBTTagCompound(Map("key" -> NBTTagDouble(1.0)))
+      val actual   = NBTTagParser.parse(input)
+      assert(actual == Right(expected))
+    }
+    it("should parse NBTTagDouble with non suffix") {
+      val input    = "{key:-1.0}"
+      val expected = NBTTagCompound(Map("key" -> NBTTagDouble(-1.0)))
+      val actual   = NBTTagParser.parse(input)
+      assert(actual == Right(expected))
+    }
+    it("should parse NBTTagDouble with non suffix / without integer part") {
+      val input    = "{key:.25}"
+      val expected = NBTTagCompound(Map("key" -> NBTTagDouble(0.25)))
+      val actual   = NBTTagParser.parse(input)
+      assert(actual == Right(expected))
+    }
+    it("should parse NBTTagString") {
+      val input    = "{key:\"1\"}"
+      val expected = NBTTagCompound(Map("key" -> NBTTagString("1")))
+      val actual   = NBTTagParser.parse(input)
+      assert(actual == Right(expected))
+    }
+    it("should parse NBTTagByteArray") {
+      val input    = "{key:[B; 1b,2b]}"
+      val expected = NBTTagCompound(Map("key" -> NBTTagByteArray(Vector(NBTTagByte(1), NBTTagByte(2)))))
+      val actual   = NBTTagParser.parse(input)
+      assert(actual == Right(expected))
+    }
+    it("should parse NBTTagIntArray") {
+      val input    = "{key:[I; 1,2]}"
+      val expected = NBTTagCompound(Map("key" -> NBTTagIntArray(Vector(NBTTagInt(1), NBTTagInt(2)))))
+      val actual   = NBTTagParser.parse(input)
+      assert(actual == Right(expected))
+    }
+    it("should parse NBTTagLongArray") {
+      val input    = "{key:[L;1L,2L]}"
+      val expected = NBTTagCompound(Map("key" -> NBTTagLongArray(Vector(NBTTagLong(1), NBTTagLong(2)))))
+      val actual   = NBTTagParser.parse(input)
+      assert(actual == Right(expected))
+    }
+    it("should parse NBTTagList with empty") {
+      val input    = "{key:[]}"
+      val expected = NBTTagCompound(Map("key" -> NBTTagList(None)))
+      val actual   = NBTTagParser.parse(input)
+      assert(actual == Right(expected))
+    }
+    it("should parse NBTTagList with NBTTagByte") {
+      val input    = "{key:[1b]}"
+      val expected = NBTTagCompound(Map("key" -> listFrom(NBTNel.Byte(NonEmptyList.of(NBTTagByte(1))))))
+      val actual   = NBTTagParser.parse(input)
+      assert(actual == Right(expected))
+    }
+    it("should parse NBTTagList with nested NBTTagList") {
+      val input    = """{key:[[1b,2b],["3","4"]]}"""
+      val expected = NBTTagCompound(
+        Map(
+          "key" -> listFrom(
+            NBTNel.List(
+              NonEmptyList.of(
+                listFrom(NBTNel.Byte(NonEmptyList.of(NBTTagByte(1), NBTTagByte(2)))),
+                listFrom(NBTNel.String(NonEmptyList.of(NBTTagString("3"), NBTTagString("4"))))
+              )
+            )
+          )
+        )
+      )
+      val actual   = NBTTagParser.parse(input)
+      assert(actual == Right(expected))
+    }
+    it("should parse NBTTagCompound") {
+      val input    = "{}"
+      val expected = NBTTagCompound(Map.empty)
+      val actual   = NBTTagParser.parse(input)
+      assert(actual == Right(expected))
+    }
+  }
+}
--- src/test/scala/nbt/definition/NBTPathTest.scala
+++ src/test/scala/nbt/definition/NBTPathTest.scala
@@ -3,11 +3,175 @@ package nbt.definition
 import org.scalatest.funspec.AnyFunSpec
 
 import scala.language.adhocExtensions
+import com.github.chencmd.lootcontainerutil.nbt.definition.NBTTag
+import com.github.chencmd.lootcontainerutil.nbt.definition.NBTPath
+import com.github.chencmd.lootcontainerutil.nbt.definition.NBTPathRootNode as RootNode
+import com.github.chencmd.lootcontainerutil.nbt.definition.NBTPathNode as Node
+import com.github.chencmd.lootcontainerutil.nbt.definition.NBTNel
+import cats.data.NonEmptyList
 
 class NBTPathTest extends AnyFunSpec {
-  describe("") {
-    it("") {
-
+  import NBTTag.*
+  describe("NBTPath") {
+    it("should be match root") {
+      val tag: NBTTagCompound = NBTTagCompound(Map("foo" -> NBTTagInt(1), "bar" -> NBTTagInt(2)))
+      val path                = NBTPath(RootNode.MatchRootObject(NBTTagCompound(Map("foo" -> NBTTagInt(1)))), List())
+      val expected            = List(NBTTagCompound(Map("foo" -> NBTTagInt(1), "bar" -> NBTTagInt(2))))
+      assert(path.access(tag) == expected)
+    }
+    it("should be not match root") {
+      val tag: NBTTagCompound = NBTTagCompound(Map("foo" -> NBTTagInt(1), "bar" -> NBTTagInt(2)))
+      val path                = NBTPath(RootNode.MatchRootObject(NBTTagCompound(Map("foo" -> NBTTagInt(2)))), List())
+      val expected            = List()
+      assert(path.access(tag) == expected)
+    }
+    it("should be get child") {
+      val tag: NBTTagCompound = NBTTagCompound(Map("foo" -> NBTTagInt(1), "bar" -> NBTTagInt(2)))
+      val path                = NBTPath(RootNode.CompoundChild("foo"), List())
+      val expected            = List(NBTTagInt(1))
+      assert(path.access(tag) == expected)
+    }
+    it("should be match object") {
+      val tag: NBTTagCompound = NBTTagCompound(
+        Map("foo" -> NBTTagCompound(Map("bar" -> NBTTagInt(1), "baz" -> NBTTagInt(2))))
+      )
+      val path                = NBTPath(RootNode.MatchObject("foo", NBTTagCompound(Map("bar" -> NBTTagInt(1)))), List())
+      val expected            = List(NBTTagCompound(Map("bar" -> NBTTagInt(1), "baz" -> NBTTagInt(2))))
+      assert(path.access(tag) == expected)
+    }
+    it("should be not match object") {
+      val tag: NBTTagCompound = NBTTagCompound(
+        Map("foo" -> NBTTagCompound(Map("bar" -> NBTTagInt(1), "baz" -> NBTTagInt(2))))
+      )
+      val path                = NBTPath(RootNode.MatchObject("foo", NBTTagCompound(Map("bar" -> NBTTagInt(2)))), List())
+      val expected            = List()
+      assert(path.access(tag) == expected)
+    }
+    it("should be match object and get child") {
+      val tag: NBTTagCompound = NBTTagCompound(
+        Map("foo" -> NBTTagCompound(Map("bar" -> NBTTagInt(1), "baz" -> NBTTagInt(2))))
+      )
+      val path                = NBTPath(
+        RootNode.MatchObject("foo", NBTTagCompound(Map("bar" -> NBTTagInt(1)))),
+        List(Node.CompoundChild("bar"))
+      )
+      val expected            = List(NBTTagInt(1))
+      assert(path.access(tag) == expected)
+    }
+    it("should be get child with all elements") {
+      val tag: NBTTagCompound = NBTTagCompound(
+        Map("foo" -> NBTTag.listFrom(NBTNel.Int(NonEmptyList.of(NBTTagInt(1), NBTTagInt(2)))))
+      )
+      val path                = NBTPath(RootNode.CompoundChild("foo"), List(Node.AllElements()))
+      val expected            = List(NBTTagInt(1), NBTTagInt(2))
+      assert(path.access(tag) == expected)
+    }
+    it("should be get child with all elements child") {
+      val tag: NBTTagCompound = NBTTagCompound(
+        Map(
+          "foo" -> NBTTag.listFrom(
+            NBTNel.Compound(
+              NonEmptyList.of(
+                NBTTagCompound(Map("bar" -> NBTTagInt(1), "baz" -> NBTTagInt(1))),
+                NBTTagCompound(Map("bar" -> NBTTagInt(2), "baz" -> NBTTagInt(1)))
+              )
+            )
+          )
+        )
+      )
+      val path                = NBTPath(
+        RootNode.CompoundChild("foo"),
+        List(Node.AllElements(), Node.CompoundChild("bar"))
+      )
+      val expected            = List(NBTTagInt(1), NBTTagInt(2))
+      assert(path.access(tag) == expected)
+    }
+    it("should be get child with match element") {
+      val tag: NBTTagCompound = NBTTagCompound(
+        Map(
+          "foo" -> NBTTag.listFrom(
+            NBTNel.Compound(
+              NonEmptyList.of(
+                NBTTagCompound(Map("bar" -> NBTTagInt(1), "baz" -> NBTTagInt(1))),
+                NBTTagCompound(Map("bar" -> NBTTagInt(2), "baz" -> NBTTagInt(1)))
+              )
+            )
+          )
+        )
+      )
+      val path                = NBTPath(
+        RootNode.CompoundChild("foo"),
+        List(Node.MatchElement(NBTTagCompound(Map("bar" -> NBTTagInt(2)))))
+      )
+      val expected            = List(NBTTagCompound(Map("bar" -> NBTTagInt(2), "baz" -> NBTTagInt(1))))
+      assert(path.access(tag) == expected)
+    }
+    it("should be get child with match elements") {
+      val tag: NBTTagCompound = NBTTagCompound(
+        Map(
+          "foo" -> NBTTag.listFrom(
+            NBTNel.Compound(
+              NonEmptyList.of(
+                NBTTagCompound(Map("bar" -> NBTTagInt(1), "baz" -> NBTTagInt(2))),
+                NBTTagCompound(Map("bar" -> NBTTagInt(2), "baz" -> NBTTagInt(1))),
+                NBTTagCompound(Map("bar" -> NBTTagInt(3), "baz" -> NBTTagInt(2)))
+              )
+            )
+          )
+        )
+      )
+      val path                = NBTPath(
+        RootNode.CompoundChild("foo"),
+        List(Node.MatchElement(NBTTagCompound(Map("baz" -> NBTTagInt(2)))))
+      )
+      val expected            = List(
+        NBTTagCompound(Map("bar" -> NBTTagInt(1), "baz" -> NBTTagInt(2))),
+        NBTTagCompound(Map("bar" -> NBTTagInt(3), "baz" -> NBTTagInt(2)))
+      )
+      assert(path.access(tag) == expected)
+    }
+    it("should be get child with indexed element") {
+      val tag: NBTTagCompound = NBTTagCompound(
+        Map(
+          "foo" -> NBTTag.listFrom(
+            NBTNel.Compound(
+              NonEmptyList.of(
+                NBTTagCompound(Map("bar" -> NBTTagInt(1), "baz" -> NBTTagInt(2))),
+                NBTTagCompound(Map("bar" -> NBTTagInt(2), "baz" -> NBTTagInt(1))),
+                NBTTagCompound(Map("bar" -> NBTTagInt(3), "baz" -> NBTTagInt(2)))
+              )
+            )
+          )
+        )
+      )
+      val path                = NBTPath(
+        RootNode.CompoundChild("foo"),
+        List(Node.IndexedElement(1))
+      )
+      val expected            = List(NBTTagCompound(Map("bar" -> NBTTagInt(2), "baz" -> NBTTagInt(1)))
+      )
+      assert(path.access(tag) == expected)
+    }
+    it("should be get child with compound child") {
+      val tag: NBTTagCompound = NBTTagCompound(
+        Map(
+          "foo" -> NBTTagCompound(
+            Map(
+              "bar" -> NBTTagInt(1),
+              "baz" -> NBTTagInt(2),
+              "qux" -> NBTTagCompound(Map("quux" -> NBTTagInt(3))
+              )
+            )
+          )
+        )
+      )
+      val path                = NBTPath(
+        RootNode.CompoundChild("foo"),
+        List(Node.CompoundChild("qux"))
+      )
+      val expected            = List(NBTTagCompound(Map("quux" -> NBTTagInt(3)))
+      )
+      assert(path.access(tag) == expected)
     }
   }
 }
