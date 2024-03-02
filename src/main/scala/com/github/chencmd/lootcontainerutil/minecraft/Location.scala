ac55633da6254f97ddc4231e00a6960deb4b467e
ChenCMD <scnmed.g@gmail.com>
2024-03-02 15:39:45 +0900

    :construction: Vector と Location のデータの持ち方を変更

--- src/main/scala/com/github/chencmd/lootcontainerutil/minecraft/Location.scala
+++ src/main/scala/com/github/chencmd/lootcontainerutil/minecraft/Location.scala
@@ -1,23 +1,47 @@
 package com.github.chencmd.lootcontainerutil.minecraft
 
+import scala.math.Fractional.Implicits.*
+import scala.math.Integral.Implicits.*
+import scala.math.Numeric.Implicits.*
 import scala.util.chaining.*
 
 import org.bukkit.Location as BukkitLocation
 import org.bukkit.World
 
-case class Location(w: World, x: Double, y: Double, z: Double) {
-  def +(other: Vector): Location = Location(w, x + other.x, y + other.y, z + other.z)
-  def -(other: Vector): Location = Location(w, x - other.x, y - other.y, z - other.z)
-  def *(other: Vector): Location = Location(w, x * other.x, y * other.y, z * other.z)
-  def /(other: Vector): Location = Location(w, x / other.x, y / other.y, z / other.z)
-  def *(n: Double): Location     = Location(w, x * n, y * n, z * n)
-  def /(n: Double): Location     = Location(w, x / n, y / n, z / n)
-  def normalize: Location        = Math.sqrt(x * x + y * y + z * z).pipe(mag => Location(w, x / mag, y / mag, z / mag))
+case class Location[A: Numeric](w: World, x: A, y: A, z: A) {
+  infix def +(other: Location[A]): Location[A] = Location(w, x + other.x, y + other.y, z + other.z)
+  infix def +(vec: Vector[A]): Location[A]     = Location(w, x + vec.x, y + vec.y, z + vec.z)
 
-  def toBukkit: BukkitLocation = new BukkitLocation(w, x, y, z)
+  infix def -(other: Location[A]): Location[A] = Location(w, x - other.x, y - other.y, z - other.z)
+  infix def -(vec: Vector[A]): Location[A]     = Location(w, x - vec.x, y - vec.y, z - vec.z)
+
+  infix def *(other: Location[A]): Location[A] = Location(w, x * other.x, y * other.y, z * other.z)
+  infix def *(vec: Vector[A]): Location[A]     = Location(w, x * vec.x, y * vec.y, z * vec.z)
+
+  infix def /(other: Location[A])(using I: Integral[A]): Location[A]   = Location(w, x / other.x, y / other.y, z / other.z)
+  infix def /(other: Location[A])(using F: Fractional[A]): Location[A] = Location(w, x / other.x, y / other.y, z / other.z)
+  infix def /(vec: Vector[A])(using I: Integral[A]): Location[A]       = Location(w, x / vec.x, y / vec.y, z / vec.z)
+  infix def /(vec: Vector[A])(using F: Fractional[A]): Location[A]     = Location(w, x / vec.x, y / vec.y, z / vec.z)
+
+  infix def *(n: A): Location[A] = Location(w, x * n, y * n, z * n)
+
+  infix def /(n: A)(using I: Integral[A]): Location[A]   = Location(w, x / n, y / n, z / n)
+  infix def /(n: A)(using F: Fractional[A]): Location[A] = Location(w, x / n, y / n, z / n)
+
+  def normalize: Location[Double] = {
+    val dx = x.toDouble
+    val dy = y.toDouble
+    val dz = z.toDouble
+    Math.sqrt(dx * dx + dy * dy + dz * dz).pipe(mag => Location(w, dx / mag, dy / mag, dz / mag))
+  }
+
+  def toVector: Vector[A] = Vector(x, y, z)
+
+  def toBukkit: BukkitLocation = new BukkitLocation(w, x.toDouble, y.toDouble, z.toDouble)
 }
 
 object Location {
-  def apply(w: World, x: Double, y: Double, z: Double): Location = new Location(w, x, y, z)
-  def apply(l: BukkitLocation): Location                         = new Location(l.getWorld, l.getX, l.getY, l.getZ)
+  def apply[A: Numeric](w: World, x: A, y: A, z: A): Location[A] = new Location(w, x, y, z)
+  def of(l: BukkitLocation): Location[Double]                    = new Location(l.getWorld, l.getX, l.getY, l.getZ)
+  def ofInt(l: BukkitLocation): Location[Int] = new Location(l.getWorld, l.getX.toInt, l.getY.toInt, l.getZ.toInt)
 }
