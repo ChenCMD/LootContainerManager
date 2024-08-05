3ef5b540bbb6debe7e2da0d2d17e9b48dc656f50
ChenCMD <scnmed.g@gmail.com>
2024-08-06 03:28:10 +0900

    :construction: 座標系関連型の実装を修正

--- src/main/scala/com/github/chencmd/lootcontainerutil/minecraft/bukkit/BlockLocation.scala
+++ src/main/scala/com/github/chencmd/lootcontainerutil/minecraft/bukkit/BlockLocation.scala
@@ -1,9 +1,17 @@
 package com.github.chencmd.lootcontainerutil.minecraft.bukkit
 
+import com.github.chencmd.lootcontainerutil.exceptions.SystemException
+
+import cats.effect.kernel.Sync
+import cats.implicits.*
+
+import scala.jdk.CollectionConverters.*
+
+import org.bukkit.Bukkit
 import org.bukkit.Location as BukkitLocation
 import org.bukkit.World
 
-final case class BlockLocation(w: World, x: Int, y: Int, z: Int) {
+final case class BlockLocation(w: String, x: Int, y: Int, z: Int) {
   infix def +(other: Position): Position = Position(w, x + other.x, y + other.y, z + other.z)
   infix def +(vec: Vector): Position     = Position(w, x + vec.x, y + vec.y, z + vec.z)
 
@@ -20,11 +28,21 @@ final case class BlockLocation(w: World, x: Int, y: Int, z: Int) {
 
   infix def /(n: Double): Position = Position(w, x / n, y / n, z / n)
 
-  def toBukkit: BukkitLocation = new BukkitLocation(w, x, y, z)
-  def toPosition: Position     = Position(w, x.toDouble, y.toDouble, z.toDouble)
-  def toVector: Vector         = Vector(x.toDouble, y.toDouble, z.toDouble)
+  def toChunkLocation: BlockLocation = BlockLocation(w, x >> 4, y >> 4, z >> 4)
+
+  def toBukkit(world: World): BukkitLocation  = new BukkitLocation(world, x, y, z)
+  def toBukkit[F[_]: Sync]: F[BukkitLocation] = for {
+    worldOpt <- Sync[F].delay(Bukkit.getWorlds().asScala.toList.find(_.getKey.toString == w))
+    world    <- worldOpt.fold(SystemException.raise(s"Missing world: $w"))(_.pure[F])
+  } yield new BukkitLocation(world, x, y, z)
+
+  def toPosition: Position = Position(w, x.toDouble, y.toDouble, z.toDouble)
+  def toVector: Vector     = Vector(x.toDouble, y.toDouble, z.toDouble)
+
+  def toXYZString: String = s"$x, $y, $z"
 }
 
 object BlockLocation {
-  def of(l: BukkitLocation): BlockLocation = new BlockLocation(l.getWorld, l.getBlockX, l.getBlockY, l.getBlockZ)
+  def of(l: BukkitLocation): BlockLocation =
+    new BlockLocation(l.getWorld.getKey.toString, l.getBlockX, l.getBlockY, l.getBlockZ)
 }
--- src/main/scala/com/github/chencmd/lootcontainerutil/minecraft/bukkit/Vector.scala
+++ src/main/scala/com/github/chencmd/lootcontainerutil/minecraft/bukkit/Vector.scala
@@ -4,6 +4,7 @@ import scala.util.chaining.*
 
 import org.bukkit.World
 import org.bukkit.util.Vector as BukkitVector
+import org.joml.Vector3f
 
 case class Vector(x: Double, y: Double, z: Double) {
   infix def +(other: Vector): Vector = Vector(x + other.x, y + other.y, z + other.z)
@@ -23,8 +24,12 @@ case class Vector(x: Double, y: Double, z: Double) {
   }
 
   def toBukkit: BukkitVector = new BukkitVector(x, y, z)
-  def toPosition(w: World): Position = Position(w, x, y, z)
-  def toBlockLocation(w: World): BlockLocation = BlockLocation(w, x.toInt, y.toInt, z.toInt)
+  def toJomlVector: Vector3f = Vector3f(x.toFloat, y.toFloat, z.toFloat)
+
+  def toPosition(w: World): Position            = Position(w.getKey.toString, x, y, z)
+  def toPosition(w: String): Position           = Position(w, x, y, z)
+  def toBlockLocation(w: World): BlockLocation  = BlockLocation(w.getKey.toString, x.toInt, y.toInt, z.toInt)
+  def toBlockLocation(w: String): BlockLocation = BlockLocation(w, x.toInt, y.toInt, z.toInt)
 }
 
 object Vector {
