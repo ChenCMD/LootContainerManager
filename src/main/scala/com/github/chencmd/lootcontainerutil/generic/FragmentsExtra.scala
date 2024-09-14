11fbfdb7491a89c9629f34b36e3d1f48e87d9613
ChenCMD <scnmed.g@gmail.com>
2024-09-14 22:58:56 +0900

    :recycle: FragmentsExtra をいい感じに

--- src/main/scala/com/github/chencmd/lootcontainerutil/generic/FragmentsExtra.scala
+++ src/main/scala/com/github/chencmd/lootcontainerutil/generic/FragmentsExtra.scala
@@ -1,6 +1,5 @@
 package com.github.chencmd.lootcontainermanager.generic
 
-import cats.Functor
 import cats.Reducible
 import cats.implicits.*
 
@@ -11,13 +10,11 @@ object FragmentsExtra {
   import Fragments.*
 
   /** Returns `(f IN ((?, ?, ..., ?), (?, ?, ..., ?), ...))` foreach the values in `fs`. */
-  def in[F[_]: Reducible: Functor, A: Write](f: Fragment, fs: F[A]): Fragment =
-    parentheses(f ++ fr"IN" ++ tupled(fs.map(tupled)))
+  def in[F[_]: Reducible, A: Write](f: Fragment, fs: F[A]): Fragment = parentheses(f ++ fr"IN" ++ tupled(fs))
 
   /** Returns `(?, ?, ..., ?)` for the values in `a` */
   def tupled[A: Write](a: A): Fragment = parentheses(values(a))
 
-  /** Returns `(fs0, fs1, ...)`. */
-  def tupled[F[_]: Reducible: Functor](fs: F[Fragment]): Fragment = parentheses(comma(fs))
-
+  /** Returns `(tupled(fs0), tupled(fs1), ...)`. */
+  def tupled[F[_]: Reducible, A: Write](fs: F[A]): Fragment = parentheses(comma(fs.toNonEmptyList.map(tupled)))
 }
